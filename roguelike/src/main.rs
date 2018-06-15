b314c4adedbe858298acb314402f92149f00b7a6
Thomas <csillangdev@gmail.com>
2018-06-15 13:05:12 -0500

    Added printing routines and bindings to make the graphics look better

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -4,6 +4,8 @@ use utils::graphics;
 
 fn main() {
 	graphics::init();
-
+	graphics::win_clear();
+	graphics::addch(5,5,'@');
+	graphics::read_char();
 	graphics::end();
 }
--- roguelike/src/utils/graphics.rs
+++ roguelike/src/utils/graphics.rs
@@ -3,17 +3,48 @@
 		fn initscr();
 		fn endwin();
 		fn getch() -> u8;
+		fn mvaddch(y:i64, x:i64, c:u8);
+		
+		fn refresh();
+		fn clear();
+
+		fn curs_set(on: i64);
+		fn cbreak();
+		fn nocbreak();
+		fn echo();
+		fn noecho();
+	}
+
+	pub fn addch(x:i64, y:i64, c: char){
+		unsafe { 
+			mvaddch(y,x, c as u8);
+			refresh();	
+		}
+	}
+
+	pub fn win_clear(){
+		unsafe { clear(); }
 	}
 
 	pub fn init(){
-		unsafe { initscr(); }
+		unsafe { 
+			initscr();
+			noecho();
+			curs_set(0);
+			cbreak();
+		 }
 	}
 
 	pub fn end(){
-		unsafe { endwin(); }
+		unsafe { 
+			echo();
+			curs_set(1);
+			nocbreak();
+			endwin(); 
+		}
 	}
 
-	pub fn readChar() -> char {
+	pub fn read_char() -> char {
 		unsafe {
 			let i = getch();
 			return i as char;
